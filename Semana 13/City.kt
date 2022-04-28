38023f6a7d8b9a382631d3b4981eb451726c83d1
Domiciano <domiciano.rincon@correounivalle.edu.co>
2022-04-28 15:46:32 -0500

    Update City.kt

--- Semana 13/City.kt
+++ Semana 13/City.kt
@@ -2,7 +2,7 @@ package edu.co.icesi.firestorestruct
 
 data class City (
     var name:String="",
-    var state:String="",
+    var state:String?="",
     var country:String="",
     var population:Int=0,
     var regions:ArrayList<String> = ArrayList()
